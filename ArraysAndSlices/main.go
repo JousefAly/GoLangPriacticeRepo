cb12b0c7fe715cbbf52cf997b3004421485b74ca
JousefAly <youssefali00286@gmail.com>
2022-05-25 03:29:52 +0200

    Added some arrays definitions

--- ArraysAndSlices/main.go
+++ ArraysAndSlices/main.go
@@ -1,6 +1,27 @@
 package main
 
 import "fmt"
-func main(){
+
+func main() {
 	fmt.Println("Hello there")
+
+	//declare array
+	var arr [5]int
+
+	arr[2] = 3
+	fmt.Println(arr[2])
+
+	var arr2 [2]int = [2]int{1, 2}
+	fmt.Println(arr2[1])
+
+	arr3 := [...]int{1, 3, 5, 7, 9}
+	fmt.Println(arr3[2])
+
+	//iterating through an array
+	// fmt.Println("Iterate an array")
+
+	// for i  , v range arr2 {
+	// 	fmt.Printf("index %d, val %d", i, v)
+	// }
+
 }
