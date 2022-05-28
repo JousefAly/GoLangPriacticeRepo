1a4bef7621ae55d2abc91e1914e9e53ed7f8c5cd
JousefAly <youssefali00286@gmail.com>
2022-05-28 17:30:23 +0200

    arguments with the same type

--- Course2/Demos/Test/hello.go
+++ Course2/Demos/Test/hello.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world")
 	multiplyPrint(4, 5)
-	fmt.Println(multiply(5,6))
+	fmt.Println(multiply2(5, 6))
 }
 
 //create a function
@@ -14,7 +14,12 @@ func multiplyPrint(x int, y int) {
 	fmt.Println(x * y)
 }
 
-func multiply(x int, y int) int{
+func multiply(x int, y int) int {
 	return x * y
 }
 
+//another way to write paramteres of the same type
+
+func multiply2(x, y int) int {
+	return x * y
+}
