e26e50cac2929d85d3b4d542900145061c2e1c81
JousefAly <youssefali00286@gmail.com>
2022-05-28 17:26:52 +0200

    create function

--- Course2/Demos/Test/hello.go
+++ Course2/Demos/Test/hello.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	fmt.Println("Hello world")
+	multiplyPrint(4, 5)
+	fmt.Println(multiply(5,6))
 }
+
+//create a function
+
+func multiplyPrint(x int, y int) {
+	fmt.Println(x * y)
+}
+
+func multiply(x int, y int) int{
+	return x * y
+}
+
