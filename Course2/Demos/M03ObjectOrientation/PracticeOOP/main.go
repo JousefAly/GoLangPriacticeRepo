9ce84a9e4305f0899b8687dfc2f2974533757046
JousefAly <youssefali00286@gmail.com>
2022-05-31 05:56:07 +0200

    added receiver pointer automatic dereferencing

--- Course2/Demos/M03ObjectOrientation/PracticeOOP/main.go
+++ Course2/Demos/M03ObjectOrientation/PracticeOOP/main.go
@@ -21,6 +21,15 @@ func (p *Point) InitMe(xn, yn float64) {
 	p.x = xn
 	p.y = yn
 }
+
+/*
+	why *Employee ? to pass by reference and not by value ok ? ok .
+	why not using dereferencing to modify the actual value ex : *e.name = name  or *e.salary = salary ?
+	because Golang compilere make it "Automatically". so no need to do this.
+	in general: Dereferencing is automatic with . operator.
+	// thanks Golang
+
+*/
 func (e *Employee) InitMe(name string, salary float64) {
 	e.name = name
 	e.salary = salary
