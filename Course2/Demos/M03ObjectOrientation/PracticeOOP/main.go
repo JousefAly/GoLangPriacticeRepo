b4bab485201d8a0d8a75d6bcf295960e69fdb22b
JousefAly <youssefali00286@gmail.com>
2022-05-31 05:35:00 +0200

    added practice InitMe with associated type Employee

--- Course2/Demos/M03ObjectOrientation/PracticeOOP/main.go
+++ Course2/Demos/M03ObjectOrientation/PracticeOOP/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"strconv"
+)
 
 // create a struct
 type Point struct {
@@ -8,9 +12,18 @@ type Point struct {
 	y float64
 }
 
+type Employee struct {
+	name   string
+	salary float64
+}
+
 func (p *Point) InitMe(xn, yn float64) {
 	p.x = xn
 	p.y = yn
+}
+func (e *Employee) InitMe(name string, salary float64) {
+	e.name = name
+	e.salary = salary
 }
 
 func main() {
@@ -22,4 +35,11 @@ func main() {
 
 	fmt.Println(point.x)
 	fmt.Println(point.y)
+
+	var employee Employee
+	employee.InitMe("Yousef", 10000.32)
+
+	fmt.Println("Hello " + employee.name)
+	fmt.Println("Your slary is: ", employee.salary)
+	fmt.Println("Your slary is: (other float format)" + strconv.FormatFloat(employee.salary, 'E', -1, 64))
 }
